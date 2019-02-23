c2a7d48ddc062606f0fabb518c7c25a05ae95e68
Stanciulescu Andrei <stanciulescu.andrei@yahoo.com>
2019-02-23 20:17:38 +0200

    Add project files.

---
+++ CSharp_GPU/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCL;
+
+namespace CSharp_GPU
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //initialize the OpenCL object
+            OpenCL.OpenCL cl = new OpenCL.OpenCL();
+            cl.Accelerator = AcceleratorDevice.GPU;
+
+            //Load the kernel
+        }
+    }
+}
